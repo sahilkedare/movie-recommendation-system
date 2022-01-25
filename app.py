ffaa76329b266d33ab5da34a6e24c3041b21d68a
Sahil Kedare <64373758+sahilkedare@users.noreply.github.com>
2022-01-25 17:20:13 +0530

    Add files via upload
    done

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=63043176d3d0fac97f25a218c7804423&language=en-US".format(movie_id)
+    response = requests.get(url)
+    data = response.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+def recommend(movie):
+     movie_index = movies[movies['title'] == movie].index[0]
+     movies_list = sorted(list(enumerate(similarity[movie_index])), reverse=True, key=lambda x: x[1])[1:6]
+     recommended_movies = []
+     recommended_movie_posters = []
+     for i in movies_list:
+          movie_id = movies.iloc[i[0]].movie_id
+          recommended_movies.append(movies.iloc[i[0]].title)
+          recommended_movie_posters.append(fetch_poster(movie_id))
+
+     return recommended_movies,recommended_movie_posters
+
+movies_dict = pickle.load(open('movies.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+# movies_list = movies_list['title'].values
+# movies= movies_list
+# print(movies_list)
+
+similarity = pickle.load(open('sim.pkl','rb'))
+
+st.title('Movies Prediction System')
+
+selected_movie_name = st.selectbox(
+     'How would you like to be contacted?',
+     movies['title'].values)
+
+if st.button('Recommend'):
+     names,posters = recommend(selected_movie_name)
+     col1, col2, col3, col4, col5 = st.columns(5)
+     with col1:
+          st.text(names[0])
+          st.image(posters[0])
+     with col2:
+          st.text(names[1])
+          st.image(posters[1])
+
+     with col3:
+          st.text(names[2])
+          st.image(posters[2])
+     with col4:
+          st.text(names[3])
+          st.image(posters[3])
+     with col5:
+          st.text(names[4])
+          st.image(posters[4])
